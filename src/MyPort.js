e32bccb3a55d88b3ac2db0a06a79b3e3af6ac7d0
divya20m <137371387+divya20m@users.noreply.github.com>
2024-04-02 15:11:23 +0530

    Update MyPort.js

--- src/MyPort.js
+++ src/MyPort.js
@@ -43,7 +43,7 @@ export function MyPort({handlefrontend,deployedhandlefrontend,textShadow}) {
   };
   const handleResume = () => {
     setResume(true);
-    window.open('https://drive.google.com/file/d/1vmJJ6HtDpVBQvEgRxv_TViMJ5F2mLZRs/view?usp=drive_link', '_self');
+    window.open('https://drive.google.com/file/d/1HcMzxi5vFF-HbSEBQJ0wVxwBrUPpJGPE/view?usp=drive_link', '_self');
   };
   return (
     <div className='My-Port' >
